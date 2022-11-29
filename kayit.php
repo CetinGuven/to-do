d979072cce228e416018541be085ef92396caa4d
Cetin Guven <117115603+CetinGuven@users.noreply.github.com>
2022-11-29 14:57:56 +0300

    Update kayit.php

--- kayit.php
+++ kayit.php
@@ -11,23 +11,23 @@ $email = $data->email;
 $password = $data->password;
 
 if (!isset($name) || !isset($email) || !isset($password)) {
-    response(false, "Lütfen eksik yerleri doldurun", false);
+    response(false, "Please fill in the missing fields", false);
 }
 
 if(($name=="") || ($email=="") || ($password==""))  {
-    response(false, "Lütfen boş bırakmayınız", false);
+    response(false, "Please fill in the missing fields", false);
 }
 
 $emailCheckQuery = $db->query("SELECT * FROM users WHERE  email='$email'");
 $emailCheckQuery->execute();
 
 if ($emailCheckQuery->rowCount() > 0) {
-    response(false, "Bu Email üzerine Hesap Bulunmaktadır", false);
+    response(false, "There is an Account on This Email", false);
 }
 
 if(!preg_match("@[0-9]+@", $password)||!preg_match("@[A-Z]+@", $password) ||!preg_match("@[a-z]+@", $password) )
 {
-    response(false, "Lütfen en az bir tane küçük/büyük harf ve rakam giriniz", false);
+    response(false, "Please enter at least one lower/uppercase letter and number", false);
 }
 
 $query = $db->prepare(
@@ -37,7 +37,7 @@ $insert = $query->execute([$name, $email, $password]);
 
 if ($query) {
     $last_id = $db->lastInsertId();
-    response($last_id, "Kayıt işleminiz yapıldı.", true);
+    response($last_id, "Your registration has been done.", true);
 }
 
 
