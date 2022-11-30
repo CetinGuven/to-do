d2615ee6d3e4e84edf92b89747be2f8f9a336240
Cetin Guven <117115603+CetinGuven@users.noreply.github.com>
2022-11-30 10:10:26 +0300

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -8,17 +8,17 @@ include ("response.php");
     $id = $_SESSION["Id"];
     try {
         if(!isset($id)){
-            response(false,"Önce Giriş Yapmalısınız",false);
+            response(false,"You Must Login First",false);
         }
         $infoQuery=$db->query("SELECT * FROM users WHERE  id='$id'");
         $infoQuery->execute();
         $datas = $infoQuery->fetchAll();
         if(count($datas) == 0){
-            response(false,"Geçersiz Session",false);
+            response(false,"Invalid Session",false);
         }else{
             response($datas[0],"",true);
         }
     } catch (\Throwable $th) {
-        response(false,"Hata Oluştu",false);
+        response(false,"An error occurred",false);
     }
 ?>
