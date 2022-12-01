b7278fe66ebdbb069a32f3e3a7be73a70ed8963e
Cetin Guven <117115603+CetinGuven@users.noreply.github.com>
2022-12-01 08:51:05 +0300

    Update update.php

--- update.php
+++ update.php
@@ -8,7 +8,6 @@ session_start();
 $json = file_get_contents("php://input");
 $data = json_decode($json);
 
-$güncelle=$data->güncelle;
 $kisi = $_SESSION["Id"];
 
 
@@ -16,14 +15,14 @@ try {
    
 $query=$db->query("UPDATE gorev_ayrıntı SET title='$güncelle' WHERE users_id='$kisi'");
     if($query>0) {
-        response(true,"Güncelleme Başarılıdır",true);
+        response(true,"Update Successful",true);
     }else{
 
-    response(false,"Güncelleme olmadı",false);
+    response(false,"Update Failed",false);
     }
 
 } catch (PDOException $e) {
-        response(false,"Hata Oluştu",false);
+        response(false,"error",false);
 
 }
  
